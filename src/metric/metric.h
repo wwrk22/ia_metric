71debeb4352990f66594f3bb5c1bdfa8d76543ea
Won Rhim <wwrk22@gmail.com>
2024-04-01 13:22:02 -0400

    comment on how Metric constructor is to be implemented by derived classes

--- src/metric/metric.h
+++ src/metric/metric.h
@@ -4,6 +4,9 @@
 class Metric {
 
 public:
+	// Require concrete derived class to clean itself up during destruction,
+	// especially when the class has been instantiated through the base Metric
+	// class.
 	virtual ~Metric() {};
 
 	int year_;
@@ -11,6 +14,9 @@ public:
 	int annual_growth_;
 
 protected:
+	// Expect a subclass to provide the values for the members.
+	// How the values are computed is up to the subclass, but the members are
+	// required to be initialized.
 	Metric(int year, int value, int annual_growth);
 };
 
--- src/metric/revenue.h
+++ src/metric/revenue.h
@@ -7,6 +7,9 @@
 class Revenue : public Metric {
 
 public:
+	// As of now, this class simply requires the values be provided by the caller
+	// of this constructor. There is no computation done by this class for value
+	// or annual_growth.
 	Revenue(int year, int value, int annual_growth);
 	~Revenue() override;
 };
